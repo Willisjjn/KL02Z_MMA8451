8a3121af06d03d8952011a85f79e41a1fcc592c8
Willis jose jimenez naranjo <willisjimenezjn@unimagdalena.edu.co>
2021-01-28 14:16:44 -0500

    Revert "Configuracion de las posciones del acelerometro MMA8451"
    This reverts commit 810ce1ddc33a2f4a34e2c741f5e196aab6001ab4.

--- MKL02Z32xxx4_Project_mma8451/source/MKL02Z32xxx4_Project_mma8451.c
+++ MKL02Z32xxx4_Project_mma8451/source/MKL02Z32xxx4_Project_mma8451.c
@@ -24,15 +24,9 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define MMA851_I2C_DEVICE_ADDRESS	        0x1D
-#define MMA8451_WHO_AM_I_MEMORY_ADDRESS		0x0D
-#define MMA8451_X_MSB_MEMORY_ADDRESS        0x01
-#define MMA8451_X_LSB_MEMORY_ADDRESS        0x02
-#define MMA8451_Y_MSB_MEMORY_ADDRESS        0x03
-#define MMA8451_Y_LSB_MEMORY_ADDRESS        0x04
-#define MMA8451_Z_MSB_MEMORY_ADDRESS        0x05
-#define MMA8451_Z_LSB_MEMORY_ADDRESS        0x06
+#define MMA851_I2C_DEVICE_ADDRESS	0x1D
 
+#define MMA8451_WHO_AM_I_MEMORY_ADDRESS		0x0D
 
 /*******************************************************************************
  * Private Prototypes
@@ -66,49 +60,35 @@
  */
 int main(void) {
 	status_t status;
-	uint8_t  nuevo_byte_uart;
-	uint8_t  nuevo_dato_i2c;
-	uint8_t  nuevo_dato_i2c_1;
-	uint8_t  nuevo_dato_i2c_2;
-	int16_t   Dato_Acelerometro;
+	uint8_t nuevo_byte_uart;
+	uint8_t	nuevo_dato_i2c;
+
 
   	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
-
 #endif
 
     (void)uart0Inicializar(115200); //115200bps
     (void)i2c0MasterInit(100000);	//100kbps
 
     PRINTF("Usar teclado para controlar LEDs\r\n");
-    PRINTF("R enciende led ROJO - r apaga led ROJO\r\n");
-    PRINTF("A enciende led AZUL - a apaga led AZUL\r\n");
-    PRINTF("V enciende led VERDE - v apaga led VERDE\r\n");
-    PRINTF("X-x Posicion acelerometro en eje x\r\n");
-    PRINTF("Y-y Posicion acelerometro en eje y\r\n");
-	PRINTF("Z-z Posicion acelerometro en eje z\r\n");
+    PRINTF("r-R led ROJO\r\n");
+    PRINTF("v-V led VERDE\r\n");
+    PRINTF("a-A led AZUL\r\n");
 
     while(1) {
     	if(uart0CuantosDatosHayEnBuffer()>0){
-
     		status=uart0LeerByteDesdeBuffer(&nuevo_byte_uart);
-
-    		if(status == kStatus_Success){
-
-    			printf("dato:%c\r\n", nuevo_byte_uart);
-
+    		if(status==kStatus_Success){
+    			printf("dato:%c\r\n",nuevo_byte_uart);
     			switch (nuevo_byte_uart) {
-
 				case 'a':
 				case 'A':
-
 					gpioPutToggle(KPTB10);
 					break;
 
@@ -129,35 +109,12 @@ int main(void) {
 				case 'M':
 					i2c0MasterReadByte(&nuevo_dato_i2c, MMA851_I2C_DEVICE_ADDRESS, MMA8451_WHO_AM_I_MEMORY_ADDRESS);
 
-					if(nuevo_dato_i2c == 0x1A)
+					if(nuevo_dato_i2c==0x1A)
 						printf("MMA8451 encontrado!!\r\n");
 					else
 						printf("MMA8451 error\r\n");
 
 					break;
-
-				case 'X':
-			  	case 'x':
-				   i2c0MasterReadByte(&nuevo_dato_i2c_1, MMA851_I2C_DEVICE_ADDRESS, MMA8451_X_MSB_MEMORY_ADDRESS);
-				   i2c0MasterReadByte(&nuevo_dato_i2c_2, MMA851_I2C_DEVICE_ADDRESS, MMA8451_X_LSB_MEMORY_ADDRESS);
-							Dato_Acelerometro = (nuevo_dato_i2c_1 << 6 ) | (nuevo_dato_i2c_2 >> 2);
-							printf("La posicion del acelerometro en el eje X es: %d\r\n", Dato_Acelerometro);
-					break;
-				case 'Y':
-				case 'y':
-				 i2c0MasterReadByte(&nuevo_dato_i2c_1, MMA851_I2C_DEVICE_ADDRESS, MMA8451_Y_MSB_MEMORY_ADDRESS);
-				 i2c0MasterReadByte(&nuevo_dato_i2c_2, MMA851_I2C_DEVICE_ADDRESS, MMA8451_Y_LSB_MEMORY_ADDRESS);
-							Dato_Acelerometro = (nuevo_dato_i2c_1 << 6 ) | (nuevo_dato_i2c_2 >> 2);
-							printf("La posicion del acelerometro en el eje Y es: %d\r\n", Dato_Acelerometro);
-					break;
-				case 'Z':
-				case 'z':
-				 i2c0MasterReadByte(&nuevo_dato_i2c_1, MMA851_I2C_DEVICE_ADDRESS, MMA8451_Z_MSB_MEMORY_ADDRESS);
-				 i2c0MasterReadByte(&nuevo_dato_i2c_2, MMA851_I2C_DEVICE_ADDRESS, MMA8451_Z_LSB_MEMORY_ADDRESS);
-							Dato_Acelerometro = (nuevo_dato_i2c_1 << 6 ) | (nuevo_dato_i2c_2 >> 2);
-							printf("La posicion del acelerometro en el eje Z es: %d\r\n", Dato_Acelerometro);
-					break;
-
 				}
     		}else{
     			printf("error\r\n");
